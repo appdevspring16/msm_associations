f41bb3f3f3e6fcee1763748adec4ddb3594c89ac
Raghu Betina <rbetina@github.com>
2015-08-12 13:03:21 -0500

    rails new

---
+++ README.md
@@ -0,0 +1,1 @@
+# Must See Movies Associations
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store :cookie_store, key: '_must_see_movies_associations_session'
