19b1a3beb60c7ddc6edf736b104eaef38911e470
meghankinney <meghankinney@users.noreply.github.com>
2016-05-16 22:16:27 +0000

    movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Movie resource:
+  # CREATE
+  get "/movies/new", :controller => "movies", :action => "new"
+  post "/create_movie", :controller => "movies", :action => "create"
+
+  # READ
+  get "/movies", :controller => "movies", :action => "index"
+  get "/movies/:id", :controller => "movies", :action => "show"
+
+  # UPDATE
+  get "/movies/:id/edit", :controller => "movies", :action => "edit"
+  post "/update_movie/:id", :controller => "movies", :action => "update"
+
+  # DELETE
+  get "/delete_movie/:id", :controller => "movies", :action => "destroy"
+  #------------------------------
+
   # Routes for the Director resource:
   # CREATE
   get "/directors/new", :controller => "directors", :action => "new"
