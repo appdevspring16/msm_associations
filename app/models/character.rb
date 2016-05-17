8a90f645492284a2afa4a87cf9bf299109ed95a2
Leland Brewster <jlbrew3@users.noreply.github.com>
2016-05-17 11:07:48 -0500

    cleaned up

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,7 @@
 class Character < ActiveRecord::Base
   validates :movie_id, :presence => true
   validates :actor_id, :presence => true
+
+  belongs_to(:actor, :class_name => "Actor", :foreign_key => "actor_id")
+  belongs_to(:movie, :class_name => "Movie", :foreign_key => "movie_id")
 end
--- app/models/director.rb
+++ app/models/director.rb
@@ -1,3 +1,5 @@
 class Director < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => { :scope => :dob }
+
+  has_many(:movies, :class_name => "Movie", :foreign_key => "director_id")
 end
