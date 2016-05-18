72a95a7573f72131f843026c7a179c2990d74021
Hannah <hannah.meyer@chicagobooth.edu>
2016-05-18 17:19:42 -0500

    Finished Movies and Directors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,55 @@
 Rails.application.routes.draw do
+  # Routes for the Actor resource:
+  # CREATE
+  get "/actors/new", :controller => "actors", :action => "new"
+  post "/create_actor", :controller => "actors", :action => "create"
+
+  # READ
+  get "/actors", :controller => "actors", :action => "index"
+  get "/actors/:id", :controller => "actors", :action => "show"
+
+  # UPDATE
+  get "/actors/:id/edit", :controller => "actors", :action => "edit"
+  post "/update_actor/:id", :controller => "actors", :action => "update"
+
+  # DELETE
+  get "/delete_actor/:id", :controller => "actors", :action => "destroy"
+  #------------------------------
+
+  # Routes for the Movie resource:
+  # CREATE
+  get "/movies/new", :controller => "movies", :action => "new"
+  post "/create_movie", :controller => "movies", :action => "create"
+
+  # READ
+  get "/movies", :controller => "movies", :action => "index"
+  get "/movies/:id", :controller => "movies", :action => "show"
+
+  # UPDATE
+  get "/movies/:id/edit", :controller => "movies", :action => "edit"
+  post "/update_movie/:id", :controller => "movies", :action => "update"
+
+  # DELETE
+  get "/delete_movie/:id", :controller => "movies", :action => "destroy"
+  #------------------------------
+
+  # Routes for the Director resource:
+  # CREATE
+  get "/directors/new", :controller => "directors", :action => "new"
+  post "/create_director", :controller => "directors", :action => "create"
+
+  # READ
+  get "/directors", :controller => "directors", :action => "index"
+  get "/directors/:id", :controller => "directors", :action => "show"
+
+  # UPDATE
+  get "/directors/:id/edit", :controller => "directors", :action => "edit"
+  post "/update_director/:id", :controller => "directors", :action => "update"
+
+  # DELETE
+  get "/delete_director/:id", :controller => "directors", :action => "destroy"
+  #------------------------------
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
