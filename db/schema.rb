cbf6ce6296abc57a137f5a158e27c89424e0c897
shao313 <shaozhu@chicagobooth.edu>
2016-05-17 20:00:17 -0500

    third

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516221904) do
+ActiveRecord::Schema.define(version: 20160518000707) do
+
+  create_table "actors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "dob"
+    t.text     "bio"
+    t.string   "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.integer  "movie_id"
+    t.integer  "actor_id"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "directors", force: :cascade do |t|
     t.string   "name"
