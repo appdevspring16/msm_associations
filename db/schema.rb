eed93db571e1a07198b31e8e02811500d00a67c4
stephenandrewberger <stephen.berger@chicagobooth.edu>
2016-05-19 15:46:43 -0500

    stopping_point_2
    10 min into video; just generated movies

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518213304) do
+ActiveRecord::Schema.define(version: 20160519204526) do
 
   create_table "directors", force: :cascade do |t|
     t.string   "name"
     t.string   "dob"
     t.text     "bio"
     t.string   "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "year"
+    t.integer  "duration"
+    t.text     "description"
+    t.string   "image_url"
+    t.integer  "director_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
