99c7a16672ff731e35a461427cb11614023dabd2
Denis Bulanov <dbulanov@user.noreply.github.com>
2016-05-19 17:37:02 -0500

    Till clean-up

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -10,4 +10,8 @@ class Movie < ActiveRecord::Base
   # - description: no rules
   # - image_url: no rules
 
+  belongs_to(:director, :class_name => "Director", :foreign_key => "director_id")
+
+  has_many(:characters, :class_name => "Character", :foreign_key => "movie_id")
+
 end
