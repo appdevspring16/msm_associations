c711482f8a11d59d5ee38a95dd709f9a61322651
Cristina Costa <cncosta25@gmail.com>
2016-05-19 11:58:33 +0200

    updated styling and actor controller

--- config/routes.rb
+++ config/routes.rb
@@ -14,60 +14,37 @@ Rails.application.routes.draw do
 
   # DELETE
   get "/delete_director/:id", :controller => "directors", :action => "destroy"
-  #------------------------------
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
+  # Routes for the Movie resource:
+  # CREATE
+  # get "/movies/new", :controller => "movies", :action => "new"
+  # post "/create_director", :controller => "movies", :action => "create"
   #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  # # READ
+  # get "/movies", :controller => "movies", :action => "index"
+  # get "/movies/:id", :controller => "movies", :action => "show"
+  #
+  # # UPDATE
+  # get "/movies/:id/edit", :controller => "movies", :action => "edit"
+  # post "/update_movie/:id", :controller => "movies", :action => "update"
+  #
+  # # DELETE
+  # get "/delete_movie/:id", :controller => "movies", :action => "destroy"
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  # Routes for the Actor resource:
+  # CREATE
+  get "/actors/new", :controller => "actors", :action => "new"
+  post "/create_actor", :controller => "actors", :action => "create"
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+  # READ
+  get "/actors", :controller => "actors", :action => "index"
+  get "/actors/:id", :controller => "actors", :action => "show"
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+  # UPDATE
+  get "/actors/:id/edit", :controller => "actors", :action => "edit"
+  post "/update_actor/:id", :controller => "actors", :action => "update"
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  # DELETE
+  get "/delete_actor/:id", :controller => "actors", :action => "destroy"
+
 end
