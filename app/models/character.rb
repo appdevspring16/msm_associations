096ebb07fe2c6eaa1683b6da59ef3711c6a4cc3d
jmunoz0 <josemunoz@Joses-Air.zyxel.com>
2016-05-19 18:44:53 -0500

    done

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,8 +6,8 @@ class Character < ActiveRecord::Base
   #  - name: no rules
 belongs_to(:actor, :class_name=>"Actor", :foreign_key=>"actor_id")
 
+belongs_to(:movie, :class_name=>"Movie", :foreign_key=>"movie_id")
 
- 
 
 
 end
