3750ca191eb4028877baffa560b86798ad5229ac
meghankinney <meghankinney@users.noreply.github.com>
2016-05-20 02:16:50 +0000

    cleaning up

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -4,6 +4,9 @@ validates :name, :presence => true, :uniqueness => {:scope => :dob}
 
 
 
+ has_many(:characters, :class_name => "Character", :foreign_key => "actor_id")
+
+
 
 
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -4,6 +4,6 @@ class Character < ActiveRecord::Base
  validates :actor_id, :presence => true
 
   belongs_to(:actor, :class_name => "Actor", :foreign_key => "actor_id")
-
+belongs_to(:movie, :class_name => "Movie", :foreign_key => "movie_id")
 
 end
