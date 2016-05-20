ef7f45cff2851ae117ce0755e595afa9d2057c13
Matthew Keenan <mkeenan@chicagobooth.edu>
2016-05-20 13:08:15 -0500

    Helper methods

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -4,4 +4,6 @@ validates :name, presence: true, uniqueness: { scope: :dob }
 # - dob: no rules
 # - bio: no rules
 # - image_url: no rules
+
+has_many :characters, :class_name => "Character", :foreign_key => "actor_id"
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -4,4 +4,8 @@ validates :movie_id, presence: true
 # - actor_id: must be present
 validates :actor_id, presence: true
 # - name: no rules
+
+belongs_to :actor, :class_name => "Actor", :foreign_key => "actor_id"
+belongs_to :movie, :class_name => "Movie", :foreign_key => "movie_id"
+  #code
 end
