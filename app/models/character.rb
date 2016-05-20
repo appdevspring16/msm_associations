d179308a91c883783328ace8334e51586c4143b2
Diego Goyret <diegoyret@gmail.com>
2016-05-20 13:00:28 -0500

    final.version

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,6 @@
 class Character < ActiveRecord::Base
 validates :movie_id,:actor_id,:presence=> true
+belongs_to(:actor,:class_name=>"Actor",:foreign_key=>"actor_id")
 end
 # Character:
 #  - movie_id: must be present
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,6 @@ validates :director_id, :presence=> true
 validates :title,:presence=> true, :uniqueness =>{:scope=>:year}
   validates :year,numericality: { only_integer: true,greater_than_or_equal_to: 1870,less_than_or_equal_to:2050 }
 validates :duration,numericality: {only_integer: true, greater_than_or_equal_to: 0,less_than_or_equal_to:2764800 }
+belongs_to(:director,:class_name=>"Director",:foreign_key=>"director_id")
+has_many(:characters,:class_name=>"Character",:foreign_key=>"movie_id")
 end
