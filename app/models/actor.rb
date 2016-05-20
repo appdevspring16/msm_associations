666a65960b3e4185bee4cd92d0b194711aeac656
hkerstens <h.kerstens@chicagobooth.edu>
2016-05-20 18:55:15 -0500

    Think its finished

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,5 @@
 class Actor < ActiveRecord::Base
-    validates :name, presence: true, uniqueness: { scope: :dob} 
+    validates :name, presence: true, uniqueness: { scope: :dob}
+
+    has_many :characters, :class_name => "Character", :foreign_key => "actor_id"
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,5 @@
 class Character < ActiveRecord::Base
   validates :movie_id, :actor_id, presence: true
   belongs_to :actor, :class_name => "Actor", :foreign_key => "actor_id"
+  belongs_to :movie, :class_name => "Movie", :foreign_key => "movie_id"
 end
