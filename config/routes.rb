bc73d84f055a464fba4dd915441b64b4af60a7b1
hkerstens <h.kerstens@chicagobooth.edu>
2016-05-20 13:17:24 -0500

    Start

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,38 @@
 Rails.application.routes.draw do
+  # Routes for the Movie resource:
+  # CREATE
+  get "/movies/new", :controller => "movies", :action => "new"
+  post "/create_movie", :controller => "movies", :action => "create"
+
+  # READ
+  get "/movies", :controller => "movies", :action => "index"
+  get "/movies/:id", :controller => "movies", :action => "show"
+
+  # UPDATE
+  get "/movies/:id/edit", :controller => "movies", :action => "edit"
+  post "/update_movie/:id", :controller => "movies", :action => "update"
+
+  # DELETE
+  get "/delete_movie/:id", :controller => "movies", :action => "destroy"
+  #------------------------------
+
+  # Routes for the Director resource:
+  # CREATE
+  get "/directors/new", :controller => "directors", :action => "new"
+  post "/create_director", :controller => "directors", :action => "create"
+
+  # READ
+  get "/directors", :controller => "directors", :action => "index"
+  get "/directors/:id", :controller => "directors", :action => "show"
+
+  # UPDATE
+  get "/directors/:id/edit", :controller => "directors", :action => "edit"
+  post "/update_director/:id", :controller => "directors", :action => "update"
+
+  # DELETE
+  get "/delete_director/:id", :controller => "directors", :action => "destroy"
+  #------------------------------
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
