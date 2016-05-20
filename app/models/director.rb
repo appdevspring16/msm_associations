907b7acebb830e1bce7cfd9c4b10898792dccca2
Brian So <bso@chicagobooth.edu>
2016-05-20 05:51:09 -0500

    What it is so far

---
+++ app/models/director.rb
@@ -0,0 +1,6 @@
+class Director < ActiveRecord::Base
+
+validates :name, :presence => true, :uniqueness => {:scope => :dob}
+
+has_many(:movies, :class_name=>"Movie",:foreign_key =>"director_id")
+end
