bcb4b2d29728d22a89123942d3cd433b7da2df83
Maayan <maharon@>
2016-05-20 01:01:52 -0500

    MA_step 1

---
+++ app/models/actor.rb
@@ -0,0 +1,15 @@
+class Actor < ActiveRecord::Base
+
+#   - name: must be present; must be unique in combination with dob
+  validates :name, :presence => true, :uniqueness => { :scope => :dob }
+
+# - dob: no rules
+# - bio: no rules
+# - image_url: no rules
+
+  belongs_to(:character, :class_name => "Character", :foreign_key => "actor_id")
+
+  has_many(:characters, :class_name => "Character", :foreign_key => "actor_id")
+
+
+end
