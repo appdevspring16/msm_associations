ac11d9abf0d66db8624f077b00d889f034575f8f
alrarruda20 <aarruda@chicagobooth.edu>
2016-05-21 02:21:29 -0500

    Final

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,2 +1,13 @@
 class Director < ActiveRecord::Base
+  # Director:
+  #  - name: must be present; must be unique in combination with dob
+  validates :name, :presence => true, :uniqueness => { :scope => :dob }
+  #  - dob: no rules
+  #  - bio: no rules
+  #  - image_url: no rules
+
+  belongs_to(:director, :class_name => "Director", :foreign_key => "director_id")
+
+  has_many(:movies, :class_name => "Movie", :foreign_key => "director_id")
+
 end
