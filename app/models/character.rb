e6478e1e3dcc51cde90f1e0b833c411ba2ffc627
Rosa Rodriguez <rrodrig1@users.noreply.github.com>
2016-05-21 13:19:00 -0500

    Final associations

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,4 +4,8 @@ class Character < ActiveRecord::Base
 
   validates :actor_id, :presence => true
 
+  belongs_to(:actor, :class_name => "Actor", :foreign_key => "actor_id")
+
+  belongs_to(:movie, :class_name => "Movie", :foreign_key => "movie_id")
+
 end
