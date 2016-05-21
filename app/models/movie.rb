414873b1c441cea1defc9961b1a39f971ef291bc
Rosa Rodriguez <rrodrig1@users.noreply.github.com>
2016-05-21 01:30:06 -0500

    Added validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ActiveRecord::Base
+
+  validates :director_id, :presence => true
+
+  validates :title, :presence => true, :uniqueness => { :scope => :year }
+
+  validates :year, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1870, :less_than_or_equal_to => 2050 }
+  
+  validates :duration, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 2764800 }
+
 end
