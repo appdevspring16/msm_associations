0bc09745507a418542f30bbfbf4641dbc19fa402
Shana Dunnigan <dunniga5@users.noreply.github.com>
2016-05-21 11:52:03 -0500

    Add Helper Methods

--- app/models/character.rb
+++ app/models/character.rb
@@ -7,5 +7,5 @@ validates :actor_id, :presence => true
   #  - name: no rules
 
 belongs_to(:actor, :class_name => "Actor", :foreign_key => "actor_id")
-
+belongs_to(:movie, :class_name => "Movie", :foreign_key => "movie_id") 
 end
