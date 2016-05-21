27bec0277658f001a93506d0a94d3f32a21545b2
Pedro A. Diaz de Leon <pantonio3@users.noreply.github.com>
2016-05-21 02:14:27 -0500

    Made some progress on this week's assignment
    Made some progress on this week's assignment

---
+++ app/models/director.rb
@@ -0,0 +1,8 @@
+class Director < ActiveRecord::Base
+# - name: must be present; must be unique in combination with dob
+validates :name, :presence => true, :uniqueness => { :scope => :dob }
+
+# - dob: no rules
+# - bio: no rules
+# - image_url: no rules
+end
