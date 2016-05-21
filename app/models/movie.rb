fe3f93c73637e392864d7c16135589049450d187
Brian Chingono <chingono@chicagobooth.edu>
2016-05-21 17:37:12 -0500

    Final edits

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,5 +13,8 @@ validates :duration, :numericality => { :only_integer => true, :greater_than_or_
 
 # - description: no rules
 # - image_url: no rules
+belongs_to(:director, :class_name => "Director", :foreign_key => "director_id")
+
+has_many(:characters, :class_name => "Character", :foreign_key => "movie_id")
 
 end
