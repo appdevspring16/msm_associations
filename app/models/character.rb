1e674931297a939278dad8ee85b03f8304b68f02
afsaja <asaja@chicagobooth.edu>
2016-05-22 20:14:23 -0500

    Cleaning up - Helper methods

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,4 +6,6 @@ validates :movie_id, :presence => true
 validates :actor_id, :presence => true
 
 # - name: no rules
+
+belongs_to(:actor, :class_name => "Actor", :foreign_key => "actor_id")
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -16,4 +16,6 @@ class Movie < ActiveRecord::Base
 
  belongs_to(:director, :class_name => "Director", :foreign_key => "director_id")
 
+ has_many(:characters, :class_name => "Character", :foreign_key => "movie_id")
+
 end
