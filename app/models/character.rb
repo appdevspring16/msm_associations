9b038a6248ba9559ef5f42075246504da7b40fe1
dangrana <danmgrana@users.noreply.github.com>
2016-06-10 23:09:02 -0500

    corrected final

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,7 @@
 class Character < ActiveRecord::Base
   validates :movie, :presence => true
-  validates :actor, :presence => true
+validates :actor, :presence => true
 
-  belongs_to :movie
-  belongs_to :actor
+belongs_to :movie
+belongs_to :actor
 end
